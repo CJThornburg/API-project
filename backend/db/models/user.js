4283ba3224c4345a8de5cf42a06ac99cb75b5723
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-08 15:47:22 -0400

    add scope to user model for user sercurity

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -40,7 +40,12 @@ module.exports = (sequelize, DataTypes) => {
       }
     }, {
     sequelize,
-    modelName: 'User'
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   }
   );
   return User;
