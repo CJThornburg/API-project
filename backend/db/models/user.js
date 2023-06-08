187c6a5a4984890c7c332aec2f3f487ca7efbe4d
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-08 19:03:22 -0400

    add sign up route

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [4, 30],
           isNotEmail(value) {
-            if (Validator.isEmcail(value)) {
+            if (Validator.isEmail(value)) {
               throw new Error("Cannot be an email.");
             }
           }
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,32 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+    const { email, password, username } = req.body;
+    //save password
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({ email, username, hashedPassword });
+
+    // need to make safe user obj to pass to setTokenCookie function
+    const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+        user: safeUser
+    });
+}
+);
+
+
+
 
 module.exports = router;
