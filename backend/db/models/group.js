81b51e69973d944b90c8c7c78a336527e3ee538e
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-10 13:15:22 -0400

    add membership model

---
+++ backend/db/models/group.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Group.init({
+    organizerId: {
+
+      type: DataTypes.INTEGER
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    about: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    type: {
+      allowNull: false,
+      type: DataTypes.ENUM
+    },
+    private: {
+      allowNull: false,
+      type: DataTypes.BOOLEAN
+    },
+    city: {
+
+      type: DataTypes.STRING
+    },
+    state: {
+
+      type: DataTypes.STRING
+    }
+  }, {
+    sequelize,
+    modelName: 'Group',
+  });
+  return Group;
+};
