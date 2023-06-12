953406ce8faf93aab246a8cf4138a42188c7a9c6
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-12 17:43:52 -0400

    with expiresIn

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -86,7 +86,7 @@ router.get('/', (req, res) => {
             id: user.id,
             email: user.email,
             username: user.username,
-            fistName: user.firstName,
+            firstName: user.firstName,
             lastName: user.lastName
         };
         return res.json({
