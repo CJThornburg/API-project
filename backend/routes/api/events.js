fb710d13ec40dd8359f3dd2b3f459c0f2fe9aae5
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-06-13 22:43:51 -0400

    add get event details by eventId

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Event, Group, Venue } = require('../../db/models');
+const { Event, Group, Venue, EventImage, Attendance } = require('../../db/models');
 const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -35,7 +35,55 @@ router.get("/", async (req, res) => {
 
 
 
+router.get("/:eventId", async (req, res, next) => {
 
+    const { eventId } = req.params;
+
+    const event = await Event.findByPk(eventId, {
+
+        include: [
+            {
+                model: Group,
+                attributes: ["id", "name", "city", "state", "private"]
+
+            },
+            {
+                model: Venue,
+                attributes: {
+                    exclude: ["groupId", "createdAt", "updatedAt"]
+                }
+            },
+            {
+                model: EventImage,
+                attributes: {
+                    exclude: ["eventId", "createdAt", "updatedAt"]
+                }
+            },
+            { model: Attendance }
+        ],
+        attributes: {
+            exclude: ["createdAt", "updatedAt"]
+        }
+    });
+
+    if (!event) {
+        const err = new Error()
+        err.message = "Event couldn't be found"
+        err.title = "Resource Not Found"
+        err.status = 404
+        return next(err)
+    }
+    if (event.dataValues.Attendances.length) {
+        event.dataValues.numAttending = event.dataValues.Attendances.length
+    } else {
+        event.dataValues.numAttending = null
+    }
+    delete event.dataValues.Attendances
+
+
+
+    res.json(event)
+})
 
 
 
